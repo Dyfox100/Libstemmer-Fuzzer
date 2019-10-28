a17bffbcb07a4b75299856ebe8d5acfb160f0447
Dylan Fox <dylan.fox@colorado.edu>
2019-10-27 18:13:23 -0600

    adding harness code with problem in struct def

--- harness/harness.py
+++ harness/harness.py
@@ -7,11 +7,14 @@ class libstemmer_harness():
         if not path.exists(libstemmer_file_path):
             raise("Libstemmer File Path Invalid!")
         self._libstemmer_functions = ctypes.CDLL(libstemmer_file_path)
-        self._stemmer = _libstemmer_functions.sb_stemmer_new("english", None)
+        self._stemmer = self._libstemmer_functions.sb_stemmer_new("english", None)
+        self._libstemmer_functions.sb_stemmer_new.restype = ctypes.c_char_p
 
-
-    def stem_word(word):
-        raise("Not Implemented")
+    def stem_word(self, word):
+        self._buffer = ctypes.create_string_buffer(len(word))
+        self._buffer.value = bytes(word, 'utf-8')
+        self._libstemmer_functions.sb_stemmer_stem(self._stemmer, self._buffer, len(word))
 
 if __name__ == "__main__":
-    libstemmer_harness("../../libstemmer_c/libstemmer.so")
+    harness = libstemmer_harness("../libstemmer_c/libstemmer.so")
+    harness.stem_word("testing")
