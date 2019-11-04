358486de6477876b18d2cebab55f56eb7a65ef69
Dylan Fox <dylan.fox@colorado.edu>
2019-11-04 13:25:12 -0700

    Adding functionality to run stemming function in new process, so that crashing the process will not terminate python fuzzer. Adding logging for this case.

--- harness/harness.py
+++ harness/harness.py
@@ -2,7 +2,7 @@ import ctypes
 from ctypes import util
 import errno
 import os
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 
 
 class libstemmer_harness():
@@ -19,20 +19,35 @@ class libstemmer_harness():
         self._stem_word.argtypes = [ctypes.c_char_p]
         self._stem_word.restype = ctypes.c_char_p
 
-    def stem_word_and_get_error_number(self, word):
+    def _stem_word_and_get_error_number(self, word, queue):
         buffer = ctypes.create_string_buffer(len(word))
         buffer.value = bytes(word, 'utf-8')
+        stemmed_word = self._stem_word(buffer, len(word))
 
-        _result = self._stem_word(buffer, len(word))
+        result = None
+        #libstemmer returns NULL upon error, which is None in python.
+        if stemmed_word == None:
+            result = {}
+            result[word] = "Stemming Returned Error No: " + str(ctypes.get_errno())
+        queue.put(result)
 
-        result = {}
-        if _result == None:
-            result[word] = ctypes.get_errno()
-        return result
+    def test_word(self, word):
+        queue = Queue()
+        stem_process = Process(target=self._stem_word_and_get_error_number, args=(word, queue,))
+        stem_process.start()
+        stem_process.join()
+        #stemming the word crashed the process. Report which signal caused crash
+        proc_exit_code = stem_process.exitcode
+        if proc_exit_code != 0:
+            return {word: "Stemming Process Crashed with Signal: " + str(-proc_exit_code)}
+        #stemming process finished, but errors maybe present in c errorno.
+        else:
+            return queue.get()
 
 if __name__ == "__main__":
     libstemmer_harness_fpath = os.path.join(
         os.path.dirname(os.path.abspath(__file__)),
         "stem_word.so")
     harness = libstemmer_harness(libstemmer_harness_fpath)
-    print(harness.stem_word_and_get_error_number("testing"))
+    print(harness.test_word("testing"))
+    print(harness.test_word("testing122222"))
