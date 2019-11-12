c9d5c432868b729f455fa9511a4883ebf1564c2d
Dylan Fox <dylan.fox@colorado.edu>
2019-11-12 08:36:54 -0700

    adding logs folder to clean up output

--- fuzz_libstemmer.py
+++ fuzz_libstemmer.py
@@ -55,6 +55,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     if args.output == None:
-        args.output = 'fuzz_logs_' + datetime.now().strftime("%m-%d-%Y,%H:%M:%S") + '.txt'
+        args.output = 'logs/fuzz_logs_' + datetime.now().strftime("%m-%d-%Y,%H:%M:%S") + '.txt'
 
     main(args.num_runs, args.length_string, args.strategy, args.output, args.v)
